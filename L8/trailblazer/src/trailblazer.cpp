5438510e8561665e1979c40c240e09cc3bc479ab
frema245 <frema245@student.liu.se>
2016-12-12 17:34:03 +0100

    påbörjat labb 8

--- L8/trailblazer/src/trailblazer.cpp
+++ L8/trailblazer/src/trailblazer.cpp
@@ -5,16 +5,41 @@
 
 #include "costs.h"
 #include "trailblazer.h"
+#include <set>
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
+vector<Vertex*> depthHelp(BasicGraph& graph, Node* node, Vertex* end, vector<Vertex*> path);
+
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
     vector<Vertex*> path;
+    vector<Vertex*> build;
+
+    path = depthHelp(graph, start, end, build);
+
     return path;
+}
+
+vector<Vertex*> depthHelp(BasicGraph& graph, Node* node, Vertex* end, vector<Vertex*> build ) {
+
+    node->visited = true;
+    build.push_back(node);
+
+    for (Arc* arc: node->arcs) {
+        Node* current = arc->finish;
+
+        if (current == end) {
+            cout << "CAAAAT" << endl;
+            build.push_back(current);
+            return build;
+
+        } else if (!current->visited) {
+            return depthHelp(graph, current, end, build);
+        }
+    }
+
+    build.pop_back();
+
 }
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
