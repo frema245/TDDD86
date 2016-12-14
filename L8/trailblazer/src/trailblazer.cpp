9fb8d38217eaa5563dc7e2335d8c82be71432050
jesjo430 <jesper_jonsson@hotmail.se>
2016-12-14 14:12:18 +0100

    Klar, ej kollat open-loop i findCost

--- L8/trailblazer/src/trailblazer.cpp
+++ L8/trailblazer/src/trailblazer.cpp
@@ -15,7 +15,7 @@ using namespace std;
 bool depthFirstSearch_help(BasicGraph& graph, Vertex* start, Vertex* end, vector<Node*>& path)
 {
     start->visited = true;
-    start->setColor(GREEN);
+    start->setColor(GREEN); //Visited
 
     if (start == end) {
         path.push_back(start);
@@ -26,11 +26,11 @@ bool depthFirstSearch_help(BasicGraph& graph, Vertex* start, Vertex* end, vector
             if (!node->visited) {
                 if (depthFirstSearch_help(graph,  node, end, path)) {
                     path.push_back(start);
-                    return true; // is it ok to ignore a return?
+                    return true; // TODO: is it ok to ignore a return?
                 }
             }
         }
-        start->setColor(RED); //Dismissed
+        start->setColor(GRAY); //Dismissed
         return false;
     }
 }
@@ -39,7 +39,10 @@ vector<Node*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
 {
     vector<Node*> path;
     graph.resetData(); //Prev. paths' have to be removed...
+
     depthFirstSearch_help(graph,  start, end, path);
+
+    reverse(path.begin(), path.end());
     return path;
 }
 
@@ -48,6 +51,7 @@ vector<Node*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
 {
     vector<Vertex*> path;
     graph.resetData(); //still important...
+
     queue<Node*> NodesToVisit;
     NodesToVisit.push(start);
 
@@ -61,6 +65,7 @@ vector<Node*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
                 path.push_back(current);
                 current = current->previous;
             }
+            reverse(path.begin(), path.end());
             return path;
         }
 
@@ -68,6 +73,7 @@ vector<Node*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
 
         for (Node* node : graph.getNeighbors(current)) {
             if (!node->visited) {
+                node->visited = true;
                 node->previous = current;
                 NodesToVisit.push(node);
                 node->setColor(YELLOW); //Processing
@@ -75,103 +81,46 @@ vector<Node*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
 
         }
     }
+
     return path;
 }
 
+vector<Node *> findCost(BasicGraph& graph, Vertex* start, Vertex* end, bool isAStar) {
 
-vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end)
-{
     vector<Vertex*> path;
-    graph.resetData();
-    PriorityQueue<Vertex*> openset; // The set of nodes to be evaluated, starting with our location
-    openset.enqueue(start, 0.0);
-    start->previous = nullptr;
-
-    while (!openset.isEmpty()) { // While we havnt check all possible paths
-        Vertex* current = openset.dequeue();
-        current->visited = true;
-        current->setColor(GREEN); //Visited
-
-        if (current == end) {
-            while (current != nullptr) {
-                path.push_back(current);
-                current = current->previous;
-            }
-            return path;
-        }
-
-        for (Edge* edge : graph.getEdgeSet(current)) {
-            Vertex* neighbour;
-            neighbour = edge->finish;
-
-            if (neighbour->visited)
-                continue;
-
-            bool open = false;
-
-            for (Edge* e : neighbour->arcs) {
-                if (e->visited) {
-                    open = true;
-                    break;
-                }
-            }
-
-            edge->visited = true;
-            graph.getEdge(neighbour, current)->visited = true; // from the other side as well.
-
-            double temp_cost = current->cost + edge->cost;
-
-            if (!open) {
-                neighbour->previous = current;
-                neighbour->cost = temp_cost;
-                openset.enqueue(neighbour, temp_cost);
-                neighbour->setColor(YELLOW);
-
-            } else if (temp_cost < neighbour->cost) {
-                neighbour->previous = current;
-                neighbour->cost = temp_cost;
-                openset.changePriority(neighbour, temp_cost);
-            }
-        }
-    }
-    return path;
-}
-
-
-vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end)
-{
     graph.resetData(); // Important...!!!
-    vector<Vertex*> path;
-    PriorityQueue<Vertex*> openset; // The set of nodes to be evaluated.
 
-    openset.enqueue(start, 0.0);
-    start->previous = nullptr;
+    PriorityQueue<Vertex*> openSet; // The set of nodes to be evaluated.
 
-    while (!openset.isEmpty()) {
-        Vertex* current = openset.dequeue();
-        current->visited = true;
+    openSet.enqueue(start, 0.0); //
+    start->previous = nullptr; //
+
+    while (!openSet.isEmpty()) { //
+        Vertex* current = openSet.dequeue(); //
+        current->visited = true; //
         current->setColor(GREEN); //Visited
 
-        if (current == end) {
-            while (current != nullptr) {
-                path.push_back(current);
-                current = current->previous;
+        if (current == end) { //
+            while (current != nullptr) { //
+                path.push_back(current); //
+                current = current->previous; //
             }
+            reverse(path.begin(), path.end());
             return path;
         }
 
-        for (Edge* edge : graph.getEdgeSet(current)) {
+        for (Edge* edge : graph.getEdgeSet(current)) { //
             Vertex* neighbour;
-            neighbour = edge->finish;
+            neighbour = edge->finish; //
 
-            if (neighbour->visited)
+            if (neighbour->visited) //
                 continue;
 
-            bool open = false;
+            bool open = false; // No openSet --> standard
 
             for (Edge* e : neighbour->arcs) {
                 if (e->visited) {
-                    open = true;
+                    open = true; // Access to OpenSet.
                     break;
                 }
             }
@@ -180,20 +129,43 @@ vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end)
             graph.getEdge(neighbour, current)->visited = true; // Visited from both sides.
 
             double temp_cost = current->cost + edge->cost;
-            double priority_cost = temp_cost + neighbour->heuristic(end);
+
+            double variable;
+            if (isAStar) {
+                variable = temp_cost + neighbour->heuristic(end);
+            } else {
+                variable = temp_cost;
+            }
+
 
             if (!open) {
                 neighbour->previous = current;
                 neighbour->cost = temp_cost;
-                openset.enqueue(neighbour, priority_cost);
-                neighbour->setColor(YELLOW);
+                openSet.enqueue(neighbour, variable);
+                neighbour->setColor(YELLOW); // processed
 
             } else if (temp_cost < neighbour->cost) {
                 neighbour->previous = current;
                 neighbour->cost = temp_cost;
-                openset.changePriority(neighbour, priority_cost);
+                openSet.changePriority(neighbour, variable);
             }
         }
     }
-    return path;
+    return path; // Ugh....
+}
+
+
+vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end)
+{
+
+    return findCost(graph, start, end, false);
+
+}
+
+
+vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end)
+{
+
+    return findCost(graph, start, end, true);
+
 }
